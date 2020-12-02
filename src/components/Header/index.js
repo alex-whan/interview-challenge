76144268e9cde78422a2ab6b948e9ff34bdd1dba
Alexander Whan <whan.alexander@gmail.com>
2020-12-01 18:03:34 -0800

    Recolored app background, added search field

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import logo from '../../assets/logo-symbol.svg';
 
 const Header = () => {
-  return <h1>Header with search bar</h1>;
+  return (
+    <>
+      <svg>{logo}</svg>
+      <h1>Header</h1>
+      <form>
+        <input type="text" placeholder="Search"></input>
+      </form>
+    </>
+  );
 };
 
 export default Header;
