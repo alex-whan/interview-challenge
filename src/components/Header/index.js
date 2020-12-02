a9b398fc0c9f040587b2259faae5c75a53529964
Alexander Whan <whan.alexander@gmail.com>
2020-12-01 18:41:23 -0800

    Imported font in app head

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,11 +4,13 @@ import logo from '../../assets/logo-symbol.svg';
 const Header = () => {
   return (
     <>
-      <img src={logo} alt="Helpful Human Logo" />
-      <h1>Header</h1>
-      <form>
-        <input type="text" placeholder="Search"></input>
-      </form>
+      <header>
+        <img src={logo} alt="Helpful Human Logo" />
+        <h1>Header</h1>
+        <form>
+          <input type="text" placeholder="Search"></input>
+        </form>
+      </header>
     </>
   );
 };
