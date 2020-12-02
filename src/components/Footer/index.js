6c7dfe6fdd449c9a9a2990817c6a491f00b63334
Alexander Whan <whan.alexander@gmail.com>
2020-12-01 17:37:46 -0800

    Created placeholder header/footer components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <h3>Footer bar</h3>;
+};
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <h1>Header with search bar</h1>;
+};
+
+export default Header;
