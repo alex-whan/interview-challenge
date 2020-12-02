ff987b6721025f63c4f92d0a28955474ca023a5e
Alexander Whan <whan.alexander@gmail.com>
2020-12-01 17:55:02 -0800

    Built out initial app component structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Pagination from './components/Pagination';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Sidebar />
+      <Pagination />
+      <Footer />
     </div>
   );
 }
