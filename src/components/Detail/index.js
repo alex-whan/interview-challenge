bc2b3d6a5bc8af3a773f009629b7be052b616d40
Alexander Whan <whan.alexander@gmail.com>
2020-12-02 15:51:30 -0800

    Rebuilt hex ID within details component for easier access

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
-// import { colors } from '../../data/colors';
-
-// maybe pass in color hex as props
-// or fetch from dB
-// React Router seems best way to handle this
 
 const Detail = () => {
   const { hexId } = useParams();
-  // const {
-  //   params: { hexId },
-  // } = match;
-
-  console.log('HEX ID in DEETS?', `${hexId}`);
+  const hex = `#${hexId}`;
 
   // const color = colors.find(color => color.code === location.hash);
-  // const hex = location.hash;
-
-  console.log('HIT DAT DETAIL??');
-  console.log('HEX??', hexId);
 
   return (
     <>
-      <h2 style={{ color: 'white' }}>Detail view for #{hexId}</h2>
+      <h2 style={{ color: 'white', backgroundColor: hex }}>
+        Detail view for {hex}
+      </h2>
       <Link to={'/'}>Clear</Link>
     </>
   );
