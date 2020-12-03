3f01f806a5d555e35f01473a19326a41fdba903a
Alexander Whan <whan.alexander@gmail.com>
2020-12-03 13:16:49 -0800

    Continued finalizing styling for submittal

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 
 function Card({ code, normalizedCode }) {
   return (
-    <div className="color-card" style={{ backgroundColor: code }}>
-      <Link to={`/color/${normalizedCode}`}>
-        <div>
+    <Link to={`/color/${normalizedCode}`}>
+      <div className="color-card">
+        <div className="color-block" style={{ backgroundColor: code }}></div>
+        <div className="color-title">
           <h4>{code}</h4>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
 
