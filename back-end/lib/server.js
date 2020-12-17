36467fe440b9b55458eb84d727b069327b5ea742
Alexander Whan <whan.alexander@gmail.com>
2020-12-17 13:11:21 -0800

    Moving cors import

--- back-end/lib/server.js
+++ back-end/lib/server.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const express = require('express');
+const app = express();
+app.use(cors());
 require('dotenv').config();
 const cors = require('cors');
-const app = express();
 const router = require('../lib/routes/api');
 const error404 = require('./middleware/404');
 const error500 = require('./middleware/500');
 
 // GLOBAL MIDDLEWARE
 app.use(express.json());
-app.use(cors());
 app.use(router);
 
 // ERRORS
