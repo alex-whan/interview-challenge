967dfd5409ecd69e5c7017ef7aa4c4f53cacfc3c
Alexander Whan <whan.alexander@gmail.com>
2020-12-17 09:27:55 -0800

    Renamed color import in API for clarity/consistency

--- back-end/lib/routes/api.js
+++ back-end/lib/routes/api.js
@@ -2,14 +2,14 @@
 
 const express = require('express');
 const router = express.Router();
-const color = require('../models/colors/colors-model');
+const colors = require('../models/colors/colors-model');
 
 // ROUTES
 // router.post('/api/v1/:model', handlePost);
 router.get('/colors', handleGetColors);
 
 async function handleGetColors(req, res, next) {
-  let colorsToGet = await color.get();
+  let colorsToGet = await colors.get();
   res.status(200).json(colorsToGet);
   console.log('GET COLORS FUNCTION RESPONSE', colorsToGet);
 }
