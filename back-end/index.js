d0dbab3f3115b53590ca9c99b9aa8cc2c015462c
Alexander Whan <whan.alexander@gmail.com>
2020-12-16 21:16:23 -0800

    Set up index.js and Mongoose options

---
+++ back-end/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const server = require('./lib/server.js');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+
+server.start(process.env.PORT);
